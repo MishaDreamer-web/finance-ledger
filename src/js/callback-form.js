0738499bbd5ccb6009d1e1568d18add89d49ab90
MishaDreamer-web <shondayn@gmail.com>
2022-03-30 02:23:24 +0300

    Fix styles for mobile, tablet, desktop...
    1.Adds an address to all social links and parameters for the transition
    2.Fix styles hero bckgrnd image
    3.Adds some scss variables
    4.Fix styles blog section
    5.Fix styles call-back form section
    6.Fix styles examples section
    7.Fix styles features section
    8.Fix styles header section
    9.Fix styles studio section

--- src/js/callback-form.js
+++ src/js/callback-form.js
@@ -4,11 +4,15 @@ const emailInput = document.querySelector('#callback-email');
 const formBtn = document.querySelector('#callback-btn');
 const warnMsg = document.querySelector('.input__warning-message');
 
+// console.log(emailInput.value);
+// console.log(formBtn);
+
 formBtn.addEventListener('click', onFormBtnSubmit);
 
 function onFormBtnSubmit(event) {
+  event.preventDefault();
+
   if (emailInput.value === '') {
-    event.preventDefault();
     warnMsg.style.opacity = 1;
   } else {
     warnMsg.style.opacity = 0;
